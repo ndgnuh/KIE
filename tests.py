6077fbfe7f54abe1caae0901c7a76c1cde62d897
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-05-20 15:06:04 +0700

    broken bbox decoding

--- tests.py
+++ tests.py
@@ -25,14 +25,29 @@ def test_tokenization():
             return False
         return all(a[k] == b[k] for k in a)
 
+    def box_approx(x, y):
+        x = np.array(x)
+        y = np.array(y)
+        for x_i, y_i in zip(x, y):
+            if not np.all(x_i == y_i):
+                print("____")
+                print(x_i)
+                print(y_i)
+        # print(x.shape, y.shape)
+        # print(np.max(np.array(x) - np.array(y)))
+        # return np.max(np.array(x) - np.array(y)) < 10
+        pass
+
     def run_tests(tokenizer, sample: Sample):
         encoded = tokenize(tokenizer, sample)
         decoded = detokenize(tokenizer, encoded)
+        print(np.array(decoded.boxes))
+        print(np.array(sample.boxes))
         assert len(encoded["texts"]) == len(encoded["boxes"])
         assert len(encoded["texts"]) == len(encoded["classes"])
         assert eq(decoded.texts, sample.texts) or \
             "<unk>" in "".join(decoded.texts)
-        assert eq(decoded.boxes, sample.boxes)
+        assert box_approx(decoded.boxes, sample.boxes)
         assert dict_eq(decoded.classes, sample.classes)
         assert set(decoded.links) == set(sample.links)
 
