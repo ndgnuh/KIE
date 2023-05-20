70a09df291cf3680aefe2b69abd0f28c6a0c415b
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-05-20 15:05:58 +0700

    update

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from kie.trainer import TrainConfig, KieConfig, TrainConfig, Trainer
+import icecream
+
+icecream.install()
 if __name__ == "__main__":
     import torch
     from icecream import install
@@ -10,22 +13,26 @@ if __name__ == "__main__":
     # dataloader = make_dataloader("data/inv_aug_noref_noimg.json", prepare_fn(tokenizer))
 
     model_config = KieConfig(
-        backbone_name="microsoft/layoutlm-base-cased",
+        backbone_name="naver-clova-ocr/bros-base-uncased",
         word_embedding_name=tokenizer_name,
         head_dims=256,
         num_classes=15,
     )
     train_config = TrainConfig(
-        total_steps=1000,
-        validate_every=100,
+        total_steps=10000,
+        validate_every=200,
         train_data="data/inv_aug_noref_noimg.json",
         validate_data="data/val.json",
-        lr=8e-5,
+        dataloader=dict(
+            batch_size=1,
+            num_workers=2,
+        ),
+        lr=5e-5,
     )
     trainer = Trainer(train_config, model_config)
+    # trainer.model.load_state_dict(torch.load("model.pt"), strict=False)
     trainer.train()
     # model = trainer.model.eval()
-    # model.load_state_dict(torch.load("model.pt"))
 
     # batch = next(iter(trainer.train_loader))
     # with torch.no_grad():
